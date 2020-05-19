d44b327103684b4c181b3ce87caa2014999b99f3
aurelth <aurelth@icloud.com>
2020-05-19 16:17:22 -0300

    Total value calculation

--- Online Budget/Online_Budget/js/scripts.js
+++ Online Budget/Online_Budget/js/scripts.js
@@ -56,12 +56,12 @@ var camisetas = {
 // parâmetros da pesquisa
 
 var parametros_pesquisa = {
-    "quantidade": 10,
+    "quantidade": 1000,
     "cor": "colorida",
     "gola": "gola_v",
     "qualidade": "q150",
     "estampa": "com_estampa",
-    "embalagem": "bulk"
+    "embalagem": "unitaria"
 }
 
 
@@ -82,14 +82,41 @@ var parametros_pesquisa = {
 
 // Resolução do desafio:
 
-$(function(){
+$(document).ready(function(){    
 
-    // Se quiser uma sugestão dos passos a seguir para a resolução, veja mais abaixo.
-    
-});
+    function atualizar_orçamento(parametros){
+        $(".refresh-loader").show();
 
+        var quantidade = parametros.quantidade;
+        var preco_unit = camisetas[parametros.cor][parametros.gola][parametros.estampa].preco_unit;
+        var foto = "img/" + camisetas[parametros.cor][parametros.gola][parametros.estampa].foto;
 
+        var valor_total = quantidade * preco_unit;
 
+        if(parametros.qualidade == "q190"){
+            valor_total *= 1.12;
+        }
+
+        if(parametros.embalagem == "unitaria"){
+            valor_total += (quantidade * 0.15);
+        }
+
+        if(quantidade >= 1000){
+            valor_total *= 0.85;
+        }
+        else if(quantidade >= 500){
+            valor_total *= 0.90;
+        }
+        else if(quantidade >= 100){
+            valor_total *= 0.95;
+        }     
+        
+        console.log(valor_total);
+    };
+
+    atualizar_orçamento(parametros_pesquisa);
+  
+  });
 
 
 
